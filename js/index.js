39751a8bd022719fca43c1d210fa3ae2ea0411bb
Alexandr <31701404+AlexandrMonakhov@users.noreply.github.com>
2020-10-15 00:02:10 +0300

    create: mobile menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const slides = document.querySelectorAll(".history-slide"),
+  prev = document.querySelector(".history__button--prev"),
+  next = document.querySelector(".history__button--next"),
+  mobileMenuButton = document.querySelector('.menu-mobile__button'),
+  mobileMenuClose = document.querySelector('.menu-mobile__close'),
+  mobileMenu = document.querySelector('.mobile');
+let slideIndex = 1;
+
+const tabsItem = $(".tabs__item");
+const contentItem = $(".trends-cards");
+
+tabsItem.on("click", function (event) {
+  const activeContent = $(this).attr("data-target");
+  tabsItem.removeClass("tabs__item--active");
+  contentItem.removeClass("trends-cards--active");
+  $(activeContent).addClass("trends-cards--active");
+  $(this).addClass("tabs__item--active");
+});
+
+
 const testimonial = new Swiper('.testimonial-slider', {
   loop: true,
   spaceBetween: 30,
@@ -12,11 +32,6 @@ const testimonial = new Swiper('.testimonial-slider', {
   },
 });
 
-const slides = document.querySelectorAll(".history-slide"),
-  prev = document.querySelector(".history__button--prev"),
-  next = document.querySelector(".history__button--next");
-let slideIndex = 1;
-
 const showSlide = (n) => {
   slideIndex = n > slides.length ? 1 : n < 1 ? slides.length : slideIndex
 
@@ -30,6 +45,11 @@ const setSlide = (n) => {
   showSlide((slideIndex += n));
 };
 showSlide(slideIndex);
+
+
+mobileMenuButton.addEventListener('click', () => mobileMenu.classList.add('mobile--active'));
+
+mobileMenuClose.addEventListener('click', () => mobileMenu.classList.remove('mobile--active'));
 
 prev.addEventListener("click", () => setSlide(-1));
 next.addEventListener("click", () => setSlide(1));
